2a9601fa368417eceec0e51a6478c9190b006964
rick <75375055+rickoproprio@users.noreply.github.com>
2023-11-25 19:05:59 -0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -10,9 +10,9 @@ exports.TEMP_DIR = path.resolve(__dirname, "..", "assets", "temp");
 
 exports.TIMEOUT_IN_MILLISECONDS_BY_EVENT = 150; //tempo que ele vai carregar os eventos em MS
 
-exports.NUMBERS_HOSTS = ["5516988265334@s.whatsapp.net"] //Números dos donos para comandos da pasta Owner, pode ter 1 ou mais donos
+exports.NUMBERS_HOSTS = ["551140028922@s.whatsapp.net"] //Números dos donos para comandos da pasta Owner, pode ter 1 ou mais donos
 
-exports.NUMBER_BOT = "5516988265334@s.whatsapp.net" //Números do BOT nunca esquecer do @s.whatsapp.net só 1 numero
+exports.NUMBER_BOT = "551140028922@s.whatsapp.net" //Números do BOT nunca esquecer do @s.whatsapp.net só 1 numero
 
 exports.TIMEOUT_IN_MILLISECONDS_BY_SPAM = 15000; //tempo que o usario pode mandar mensagem nesse caso 2 msg a cada 15 segundos, depois disso é ignorado.
 
